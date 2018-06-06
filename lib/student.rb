b0c8fce45ccf045b76a9e676e61931e0f1a0152a
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-06-06 19:21:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -119,28 +119,28 @@ class Student
     result
   end
   
-  # def self.first_student_in_grade_10
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE grade = 10
-  #     LIMIT 1
-  #   SQL
-    
-  #   DB[:conn].execute(sql).map do |row|
-  #     self.new_from_db(row)
-  #   end
-  # end
-  
-    def self.first_student_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
+      LIMIT 1
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
+  
+  #   def self.first_student_in_grade_10
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM students
+  #     WHERE grade = 10
+  #   SQL
+    
+  #   DB[:conn].execute(sql).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
 end
