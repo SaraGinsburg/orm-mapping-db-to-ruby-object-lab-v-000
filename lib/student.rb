5058ace9fc7655149aae30609bb906464c10fc79
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-06-06 19:20:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -137,11 +137,10 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      
     SQL
     
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row).first
-    end
+      self.new_from_db(row)
+    end.first
   end
 end
