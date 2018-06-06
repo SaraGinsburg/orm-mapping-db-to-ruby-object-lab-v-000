c3b2764110f31381525e17f0fbbd9c734ae439c4
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-06-06 19:20:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -121,7 +121,7 @@ class Student
   
   # def self.first_student_in_grade_10
   #   sql = <<-SQL
-  #     SELECT 
+  #     SELECT *
   #     FROM students
   #     WHERE grade = 10
   #     LIMIT 1
@@ -134,7 +134,7 @@ class Student
   
     def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT 
+      SELECT *
       FROM students
       WHERE grade = 10
       
